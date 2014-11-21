35693d185c9d4416cd0b2913aadd848132bc0c2a
Takemi Watanuki <watakemi725@Takemi-no-MacBook-Pro.local>
2014-11-21 15:54:37 +0900

    first commit

---
+++ BtnTackle/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BtnTackle
+//
+//  Created by Takemi Watanuki on 2014/11/21.
+//  Copyright (c) 2014年 Takemi Watanuki. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BtnTackle/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BtnTackle
+//
+//  Created by Takemi Watanuki on 2014/11/21.
+//  Copyright (c) 2014年 Takemi Watanuki. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
